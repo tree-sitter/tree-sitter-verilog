8b577b1f3726c097519d9ce64a89a56ae3971109
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-03-15 17:30:07 -0700

    get real tree-sitter path

--- bin/install.js
+++ bin/install.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const cp = require('child_process');
 
 const gyp = cb => {
@@ -19,8 +20,9 @@ fs.access('src/parser.c',
   (fs.constants.F_OK | fs.constants.R_OK),
   err => {
     if (err) {
-      console.log('generate');
-      const proc = cp.spawn('tree-sitter', ['generate']);
+      const pat = path.resolve(process.cwd(), './node_modules/.bin/tree-sitter');
+      console.log('generate', pat);
+      const proc = cp.spawn(pat, ['generate']);
       proc.stderr.on('data', data => {
         console.error(data.toString());
       });
