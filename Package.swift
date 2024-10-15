f226a3c500b585cd9bd32c1d1553abf99ada2760
Amaan Qureshi <amaanq12@gmail.com>
2024-10-15 02:15:57 -0400

    build: update bindings

--- Package.swift
+++ Package.swift
@@ -3,31 +3,31 @@ import PackageDescription
 
 let package = Package(
     name: "TreeSitterVerilog",
-    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "TreeSitterVerilog", targets: ["TreeSitterVerilog"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+    ],
     targets: [
-        .target(name: "TreeSitterVerilog",
-                path: ".",
-                exclude: [
-                    "binding.gyp",
-                    "bindings",
-                    "Cargo.toml",
-                    "corpus",
-                    "examples",
-                    "grammar.js",
-                    "LICENSE",
-                    "package.json",
-                    "README.md",
-                    "src/grammar.json",
-                    "src/node-types.json",
-                ],
-                sources: [
-                    "src/parser.c",
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
-    ]
+        .target(
+            name: "TreeSitterVerilog",
+            dependencies: [],
+            path: ".",
+            sources: [
+                "src/parser.c",
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterVerilogTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterVerilog",
+            ],
+            path: "bindings/swift/TreeSitterVerilogTests"
+        )
+    ],
+    cLanguageStandard: .c11
 )
